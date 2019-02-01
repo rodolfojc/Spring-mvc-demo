f16dc011450813707323f0ee60da666f7485ffae
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-01 14:03:26 +0000

    HelloWordController and Form
    Added

--- WebContent/WEB-INF/view/main-menu.jsp
+++ WebContent/WEB-INF/view/main-menu.jsp
@@ -1,4 +1,4 @@
-<!DOCTYPE HTLM>
+<!DOCTYPE html>
 <html>
 
 <body>
---
+++ src/com/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	//CONTROLLER METHOD TO SHOW INITIAL FORM
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	//CONTROLLER METHOD TO PROCESS THE HTML FORM
+	
+	
+	
+}
