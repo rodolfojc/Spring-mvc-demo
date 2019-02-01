417e7df139098b0b112eb1d33847535916ebe0eb
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-01 17:57:28 +0000

    Request Param
    Demo

--- src/com/springdemo/mvc/HelloWorldController.java
+++ src/com/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -34,6 +35,21 @@ public class HelloWorldController {
 		
 		//CREATE THE MESSAGE
 		String result = "Yo! "+theName;
+		
+		//ADD MESSAGE TO MODEL 
+		model.addAttribute("message", result);
+		
+		return"helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+				
+		//CONVERT THE DATA TO ALL CAPITAL
+		theName = theName.toUpperCase();
+		
+		//CREATE THE MESSAGE
+		String result = "Wow, you code! "+theName;
 		
 		//ADD MESSAGE TO MODEL 
 		model.addAttribute("message", result);
