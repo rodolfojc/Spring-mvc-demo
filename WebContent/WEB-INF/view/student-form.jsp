ff77eda0a95fb7c9a82fb844471c68c9a82e305d
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-02 21:17:27 +0000

    HTML - Drop-Down List
    Demo

--- WebContent/WEB-INF/view/student-form.jsp
+++ WebContent/WEB-INF/view/student-form.jsp
@@ -17,6 +17,14 @@
 		Last name: <form:input path="lastName"/>
 		<br>
 		<br>
+		Country
+		<form:select path="country">
+			<form:option value="Venezuela" label="Venezuela"/>
+			<form:option value="France" label="France"/>
+			<form:option value="Germany" label="Germany"/>
+			<form:option value="India" label="India"/>
+		</form:select>
+		
 		<input type="submit" value="Submit">
 	
 	</form:form>
--- src/com/springdemo/mvc/Student.java
+++ src/com/springdemo/mvc/Student.java
@@ -4,11 +4,12 @@ public class Student {
 
 	private String firstName;
 	private String lastName;
+	private String country;
 	
 	public Student() {
 		
 	}
-
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -24,6 +25,14 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 	
-	
+		
 }
